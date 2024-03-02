5215aa1d553a73f2e5895292cb092e26d8b0f6b4
Stephan de Vries <stephan281094@gmail.com>
2024-03-02 14:33:04 +0100

    Add NARD endpoint

--- api/chapters.js
+++ api/chapters.js
@@ -1,20 +1,5 @@
 import fetch from "node-fetch";
-
-/**
- * Convert a chapter to GeoJSON, so it can be used in maps.
- *
- * @param {Object} chapter
- */
-function toGeoJson(chapter) {
-  return {
-    type: "Feature",
-    geometry: {
-      type: "Point",
-      coordinates: [chapter.Lng, chapter.Lat],
-    },
-    properties: chapter,
-  };
-}
+import { toGeoJson } from "../lib/geojson";
 
 /**
  * Fetch DxE chapters and parse the response to JSON.
